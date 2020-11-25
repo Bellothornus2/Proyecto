74b8fcc4e01de393d70149bda9c4f7a4e936da31
bellothornus2 <divanov@cifpfbmoll.eu>
2020-11-25 17:57:57 +0100

    refactor(ProyectoScraping): :art: We separate the crawler from the main.py code
    We separate the code in functionalities grouped by categories (like webscraper and webcrawler)
    
    Erros,None

--- Src/code.py
+++ Src/code.py
@@ -1,6 +1,6 @@
 #Here we import the library to download the HTMl
 import urllib.request
-
+from . import web_crawler
 #we initialize the empty list "list_links" to store all the links here
 #and the varaible string "string_url" to store the root of the content
 #we initialize the dictionary who contains the ids of the elements that we want to store 
@@ -62,45 +62,8 @@ def css_class_finder(page,key):
         string_class = page[start_quote + 1: end_quote]
         """
 
-#This function gets the next link from a given position from the html
-def get_next_link(page):
-    #encuentra el primer enlace desde la posición 0 
-    #del parámetro "page"
-    start_link = page.find('<a href=')
-    #si no lo encuetra me asigna nada en la URL
-    #y 0 en la posición
-    if start_link == -1:
-        string_url= None
-        end_quote = 0
-    #si lo encuentra, entonces me busca el enlace entre la etiqueta
-    #<a> HTML, cambia el parámatero
-    else:
-        start_quote = page.find('"', start_link)
-        end_quote = page.find('"', start_quote + 1)
-        string_url= page[start_quote + 1:end_quote]
-    return string_url, end_quote
 
-#This function Stores all the Links contained in a single HTML File (in this case HTML page)
-def print_all_links(page):
-    while True:
-        string_url, endpos = get_next_link(page)
-        if string_url:
-            if string_url == '#' or string_url in list_links or ".." in string_url:
-                page = page[endpos+1:]
-                continue
-            else:
-                list_links.append(string_url)
-                page = page[endpos:]
-        else:
-            break
 
-#This function Stores all the links from the webpage visiting all the links recursively
-#discriminating the duplicates and those containing ".." in it
-def get_all_pages(list_links):
-    for string_url in list_links:
-        page = gethtml(string_url)
-        print_all_links(page)
-        list_links = list(set(list_links))
 
 #Here we take all th links from the root level "index.html"
 page = gethtml(string_url)
