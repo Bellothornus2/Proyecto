a784f76901c7ae18b83554b2ab52daef2ec96b7f
bellothornus2 <divanov@cifpfbmoll.eu>
2020-12-04 21:01:44 +0100

    feat(ProyectoScraping): :sparkles: we added the save_json_file function and save_json module 2 ways of saving data

--- Src/main.py
+++ Src/main.py
@@ -1,5 +1,8 @@
-from modules.web_crawler import get_html, get_all_pages
-from modules.web_scraper import css_class_finder
+from modules.web_crawler import get_all_pages
+from modules.web_scraper import get_all_content
+from modules.json.save_json import save_json_file
+
+
 #we initialize the empty list "list_links" to store all the links here
 #and the varaible string "string_url" to store the root of the content
 #we initialize the dictionary who contains the ids of the elements that we want to store 
@@ -13,6 +16,7 @@ list_class_html = [
     "HasCupon",
     "HasParking"
 ]
+#este diccionario coge los registros de cada columna
 dict_class_html = {
     "PricePack":[],
     "NamePack":[],
@@ -20,17 +24,36 @@ dict_class_html = {
     "HasCupon":[],
     "HasParking":[]
 }
-#dict_class_html = dict.fromkeys(list_class_html, [])
+"""
+dict_class_html_experimental = {
+    "0":{
+        "PricePack":"Price",
+        "NamePack":"Name",
+        "ContentPack":"Content"
+    },
+    "1":{
+        "pricePack":"Price"
+    }
+}
+"""
+dict_class_html_experimental = {}
+#list_links = get_all_pages(webpage=webpage)
+#print(list_links)
 
-list_links = get_all_pages(webpage=webpage)
-print(list_links)
 
-#Here we take all the content of the pages
-def get_all_content(list_links, list_class_html, dict_class_html):
-    for url in list_links:
-        page = get_html(url)
-        for class_html in list_class_html:
-            dict_class_html = css_class_finder(page,class_html, dict_class_html)
-    return dict_class_html
+#dict_class_html = get_all_content(list_links, list_class_html, dict_class_html)
+dict_class_html =  get_all_content(["products.html"],list_class_html,dict_class_html)
+#This function stores all the content of the pages in a json file per column
+#save_json_file(get_all_content(get_all_pages(webpage=webpage),list_class_html,dict_class_html))
+#this function stores all the content of the pages in a json file per pack
+for i in range(len(dict_class_html)+1):
+    dict_class_html_experimental[str(i)] = {}
+    dict_class_html_experimental[str(i)]["PricePack"] = dict_class_html["PricePack"][i]
+    dict_class_html_experimental[str(i)]["NamePack"] = dict_class_html["NamePack"][i]
+    dict_class_html_experimental[str(i)]["ContentPack"] = dict_class_html["ContentPack"][i]
+    dict_class_html_experimental[str(i)]["HasCupon"] = dict_class_html["HasCupon"][i]
+    dict_class_html_experimental[str(i)]["HasParking"] = dict_class_html["HasParking"][i]
 
-dict_class_html = get_all_content(list_links, list_class_html, dict_class_html)
+save_json_file(dict_class_html_experimental)
+
+
