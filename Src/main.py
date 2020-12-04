e0aab986833c0b74e2506a247755e29d31a969a1
bellothornus2 <divanov@cifpfbmoll.eu>
2020-12-04 19:55:47 +0100

    fix(ProyectoScraping): :bug: we fixed the bad functionality of the program

--- Src/main.py
+++ Src/main.py
@@ -1,4 +1,4 @@
-from modules.web_crawler import get_all_links, get_html, get_all_pages
+from modules.web_crawler import get_html, get_all_pages
 from modules.web_scraper import css_class_finder
 #we initialize the empty list "list_links" to store all the links here
 #and the varaible string "string_url" to store the root of the content
@@ -13,16 +13,24 @@ list_class_html = [
     "HasCupon",
     "HasParking"
 ]
-dict_class_html = dict.fromkeys(list_class_html, [])
+dict_class_html = {
+    "PricePack":[],
+    "NamePack":[],
+    "ContentPack":[],
+    "HasCupon":[],
+    "HasParking":[]
+}
+#dict_class_html = dict.fromkeys(list_class_html, [])
 
 list_links = get_all_pages(webpage=webpage)
 print(list_links)
 
 #Here we take all the content of the pages
-for url in list_links:
-    page = get_html(url)
-    for class_html in list_class_html:
-        list_content = css_class_finder(page,class_html)
-        if list_content != None:
-            dict_class_html[class_html].append(list_content)
-print(dict_class_html)
+def get_all_content(list_links, list_class_html, dict_class_html):
+    for url in list_links:
+        page = get_html(url)
+        for class_html in list_class_html:
+            dict_class_html = css_class_finder(page,class_html, dict_class_html)
+    return dict_class_html
+
+dict_class_html = get_all_content(list_links, list_class_html, dict_class_html)
