b5b45b154f09202bbcda4bde36d718112c46a17d
bellothornus2 <divanov@cifpfbmoll.eu>
2020-12-08 16:48:03 +0100

    fix(ProyectoScraping): :bug: we fixed the pretty_json_homemade replacement of strings

--- Src/modules/json/pretty_json_homemade.py
+++ Src/modules/json/pretty_json_homemade.py
@@ -2,7 +2,7 @@ def pretty_json_homemade(dict_class_html):
     data = str(dict_class_html)
     data = data.replace("],","],\n")
     data = data.replace("[","[\n\t")
-    data = data.replace("',",",\n\t")
+    data = data.replace("',","',\n\t")
     data = data.replace("'],","'\n],")
     data = data.replace("']","'\n]")
     data = data.replace("{","{\n")
