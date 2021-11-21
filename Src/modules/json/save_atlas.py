4196975702e07f5dbbdbe970e98293fbb420f739
bellothornus2 <divanov@cifpfbmoll.eu>
2021-11-21 13:18:01 +0100

    refactor(multiple registers): :art: we changed the structure of how stores the data

--- Src/modules/json/save_atlas.py
+++ Src/modules/json/save_atlas.py
@@ -1,4 +1,5 @@
 import pymongo
+#import dns
 #Here we are connecting to the server
 def save_atlas(dict_class_html_row):
     client = pymongo.MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jhgbt.gcp.mongodb.net/admin?retryWrites=true&w=majority")
@@ -6,6 +7,9 @@ def save_atlas(dict_class_html_row):
     db = client.amenities
     #Here we are connecting to the collection
     collection = db.packs2
+    #print(dict_class_html_row)
     #here we are inserting the retrieved data
-    insertado = collection.insert_one(dict_class_html_row)
-    client.close()
+    for key,value in dict_class_html_row.items():
+        #print("{%s:%s}" % (key,value))
+        
+        collection.insert_one(value)
