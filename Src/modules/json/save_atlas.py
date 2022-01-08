386bf47622620dc0dad10f968dba9e00a313cc7f
bellothornus2 <divanov@cifpfbmoll.eu>
2022-01-08 19:44:50 +0100

    feat: :bug:

--- Src/modules/json/save_atlas.py
+++ Src/modules/json/save_atlas.py
@@ -2,7 +2,8 @@ import pymongo
 #import dns
 #Here we are connecting to the server
 def save_atlas(dict_class_html_row):
-    client = pymongo.MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jhgbt.gcp.mongodb.net/admin?retryWrites=true&w=majority")
+    #client = pymongo.MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jhgbt.gcp.mongodb.net/admin?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://m001-student:12345@sandbox.glkvp.mongodb.net/myFirstDatabase")
     #Here we are connecting to the database
     db = client.amenities
     #Here we are connecting to the collection
